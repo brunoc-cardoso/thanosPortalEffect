adf5b652028369db7f0888e04c8615f4f190f99e
bruno.cardoso <brunocpessoal@gmail.com>
2019-05-29 17:31:09 -0300

    made the setup of particles

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ let scene, sceneLight, cam, renderer;
 function initScene() {
   scene = new THREE.Scene();
 
-  sceneLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  sceneLight.position.set(0, 0, 1);
+  sceneLight = new THREE.DirectionalLight(0xffffff,0.5);
+  sceneLight.position.set(0,0,1);
   scene.add(sceneLight);
 
   cam = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000);
@@ -19,10 +19,25 @@ function initScene() {
   document.body.appendChild(renderer.domElement);
 
   renderer.render(scene, cam);
+  particleSetup();
 }
 
 function particleSetup() {
-  
+  let loader = new THREE.TextureLoader();
+
+  loader.load('smoke.png', function (texture) {
+    let portalGeo = new THREE.PlaneBufferGeometry(350, 350);
+    let portalMaterial = new THREE.MeshStandardMaterial({
+      map: texture,
+      transparent: true
+    });
+
+    let particle = new THREE.Mesh(portalGeo, portalMaterial);
+    particle.position.set(2, 2);
+    scene.add(particle);
+
+    renderer.render(scene, cam);
+  });
 }
 
 initScene();
